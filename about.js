6afbc82b34bcf9cf1f6e3d20a91e56bab5d0e4ed
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-06 23:26:39 -0400

    Update about.js

---
+++ about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const About = () => {
+    return (
+        <div className="about-section">
+            <h2>About Me</h2>
+            <p>
+              Hello! I am Smith Alice, a fictional character created to showcase the beauty of React. I love building web applications and learning new technologies.
+            </p>
+        </div>
+    );
+
+};
+
+export default About;
