f1b11b285bfda3d9cc6c7b7b43ca1486e83f79b1
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-06 23:27:46 -0400

    Update contact.js

---
+++ contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render () {
+        return (
+            <div className="contact-section">
+                <h2>Contact Information</h2>
+                <p>Email: smithalice@example.com</p>
+                <p>LinkedIn: <a href="https://www.linkedin.com/in/smithalice/">linkedin.com/in/smithalice</a></p>
+            </div>
+
+        );
+    }
+}
+
+export default Contact;
