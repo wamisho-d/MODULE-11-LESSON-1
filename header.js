ee82d7d068c0dc705dfc3047b223f2081f7bb32a
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-06 23:28:26 -0400

    Update header.js

---
+++ header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'; // Assuming you are adding some basic styles here
+
+const Header = () => {
+    return (
+        <div className="header-container">
+          <div className="header-background">
+            <h1 className="header-title">Smith Alice</h1>
+          </div>
+        </div>
+
+    );
+};
+
+export default Header;
